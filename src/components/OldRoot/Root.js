ccb8df9308ac11812e645cabc52d6c6ca7afa19b
Simon <bauchet.simon@gmail.com>
2020-06-05 15:53:27 +0200

    rewrite all page with clean css

---
+++ src/components/OldRoot/Root.js
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from "react";
+
+import './Root.css';
+import 'react-player';
+import ReactPlayer from "react-player";
+
+const Root = () => {
+	window.addEventListener("resize", resize);
+	const [isSmallScreenPortraitVideo, setIsSmallScreenPortraitVideo] = useState(window.innerWidth < 820 ? true : false);
+	const [isSmallScreenLandscapeVideo, setIsSmallScreenLandscapeVideo] = useState(window.innerWidth < 900 ? true : false);
+
+	function resize() {
+		console.log(window.innerWidth);
+		if (window.innerWidth < 820)
+			setIsSmallScreenPortraitVideo(true);
+		else
+			setIsSmallScreenPortraitVideo(false);
+
+		if (window.innerWidth < 900)
+			setIsSmallScreenLandscapeVideo(true);
+		else
+			setIsSmallScreenLandscapeVideo(false);
+	}
+
+
+	return (
+		<div className={"container_background"} style={{width: window.innerWidth}}>
+			<div className={"container_headband"}>
+				<button className={"text_headband"} style={{marginLeft: '0%'}}>Werewolf</button>
+				<button className={"text_headband"}>Time Bomb</button>
+				<button className={"text_headband"}>Maze Adventure</button>
+			</div>
+			<div className={"container_body"} style={{height: window.innerHeight, marginLeft: -10, width: window.innerWidth, padding: 5}}>
+				<h2 className={"name"}>Simon Bauchet</h2>
+				<div style={{backgroundColor: '#FFF', height: window.innerHeight+window.innerHeight*0.05, padding: 5, display: 'flex', width: window.innerWidth}}>
+					<p style={{color: '##7D7F7B'}}>Etudiant à Epitech en 4ème année, je suis passionné de l'informatique.</p>
+				</div>
+				<div style={{backgroundColor: '#FF4655', height: window.innerHeight+window.innerHeight *0.05, padding: 5, display: 'flex', width: window.innerWidth, flexDirection: isSmallScreenPortraitVideo ? 'column' : ''}}>
+					<div style={{marginTop: window.innerHeight*0.10, width: window.innerWidth, display: 'flex', flexDirection: isSmallScreenLandscapeVideo ? 'column' : ''}}>
+						<ReactPlayer url={'https://youtu.be/hCzs0sIr5lI'} config={{youtube: {embedOptions: {width: 300, height: 635}, playerVars: {showinfo: 0}}}} className={"video_container_portrait"} playing={true} loop={true}/>
+						<div style={{flexDirection: 'column'}}>
+							<span style={{color: '#fff', display: 'flex', marginLeft: 120, fontSize: '300%'}}>Time Bomb</span>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Technologie utilisées: NodeJs, React</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Projet réalisé en 2 semaines pendant mon temps libre.</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Le but de ce projet est de pouvoir jouer au jeux de société Time Bomb n'importe où.</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Je n'ai pas réalisé d'application pour ce projet car je veux que tout le monde puisse y jouer sans rien installer, ainsi toute personne peut se joindre en soirée à une partie par example.</p>
+						</div>
+					</div>
+				</div>
+				<div style={{backgroundColor: '#FFF', height: window.innerHeight+window.innerHeight * (isSmallScreenPortraitVideo? 0.15 : 0.05), padding: 5, display: 'flex', width: 'fit-content', flexDirection: isSmallScreenPortraitVideo ? 'column' : ''}}>
+					<div style={{marginTop: window.innerHeight* (isSmallScreenPortraitVideo?0.05:0.10), width: window.innerWidth, display: 'flex', flexDirection: isSmallScreenLandscapeVideo ? 'column' : ''}}>
+					<ReactPlayer url={'https://youtu.be/CGfeq1l48JI'} config={{youtube: {embedOptions: {width: 300, height: 635}, playerVars: {showinfo: 0}}}} className={"video_container_portrait"} playing={true} loop={true}/>
+						<div style={{flexDirection: 'column', marginTop: 20}}>
+							<p style={{color: '#768079', display: 'flex', marginLeft: 15}}>Technologie utilisées: NodeJs, React</p>
+							<p style={{color: '#768079', display: 'flex', marginLeft: 15}}>Projet réalisé en 7 jours pendant mon temps libre.</p>
+							<p style={{color: '#768079', display: 'flex', marginLeft: 15}}>Le but de ce projet est de pouvoir jouer au jeux de société Loup Garou nimporte où.</p>
+							<p style={{color: '#768079', display: 'flex', marginLeft: 15}}>Il permet d'avoir un mélangeur de rôle afin de pouvoir jouer ou que l'on est.
+								Le premier joueur à se connecter est le maître du jeu et peut décider des rôles.
+								Quand une personne meurt, elle peut dévoiler son rôle aux autres joueurs avec le bouton de révélation.</p>
+							<p style={{color: '#768079', display: 'flex', marginLeft: 15}}>Je n'ai pas réalisé d'application pour ce projet car je veux que tout le monde puisse y jouer sans rien installer, ainsi toute personne peut se joindre en soirée à une partie par example.</p>
+						</div>
+					</div>
+				</div>
+				<div style={{backgroundColor: '#0F1923', height: window.innerHeight+window.innerHeight *0.05, padding: 5, display: 'flex', width: 'fit-content', alignItems: 'center', flexDirection: isSmallScreenLandscapeVideo ? 'column' : ''}}>
+					<div style={{marginTop: window.innerHeight*0.10, width: window.innerWidth, display: 'flex', flexDirection: isSmallScreenLandscapeVideo ? 'column' : ''}}>
+					<ReactPlayer url={'https://youtu.be/tVsqJKOjwwg'} config={{youtube: {embedOptions: {width: 635, height: 300}, playerVars: {showinfo: 0}}}} className={"video_container_landscape"} playing={true} loop={true}/>
+						<div style={{flexDirection: 'column'}}>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Technologie utilisées: Unity, C#</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Projet en cours de réalisation.</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Le but du jeux est de trouver la sortie du labyrinthe afin de récolter tous les trésors</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Il y a un système de loot, et de build afin d'aider le joueur dans sa quête.</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Le jeux est en cours de réalisation, c'est pourquoi il peut y avoir des bugs et des features manquantes.</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Dans le jeux, un carré vert représente la sortie, celui de couleur bleu représente la clé de la salle secrète, le carré rouge représente la salle secrète lorsqu'elle est fermée, celle-ci passe au orange une fois la clé récupérée.</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15, fontWeight: 'bold'}}>Ce que j'aime:</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>- J'ai designé moi même le système de loot afin de pouvoir gérer le taux de drop de chaque item.</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>- Le système de build, autant la conception pour réaliser quelque chose d'équilibré que la réalisation qui réserve ses petits imprévus !</p>
+							<p style={{color: '#fff', display: 'flex', marginLeft: 15}}>Pour le tester &nbsp; <a href={'https://simmer.io/@cryan/mazeadventure'} target={'_blank'}> cliquez ici</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+};
+
+export default Root;
--- src/components/Root/Root.js
+++ src/components/Root/Root.js
@@ -22,58 +22,79 @@ const Root = () => {
 			setIsSmallScreenLandscapeVideo(false);
 	}
 
+	function div_container_phone(backgroundColor) {
+		if (isSmallScreenLandscapeVideo)
+			return {textAlign: 'center', position: 'relative', height: 'auto', minHeight: '100%', width: window.innerWidth, overflow: 'auto', paddingTop: 30, paddingBottom: 30, backgroundColor: backgroundColor}
+		else
+			return {textAlign: 'center', position: 'relative', height: 'auto', minHeight: '100%', width: window.innerWidth, overflow: 'auto', paddingTop: 30, paddingBottom: 30, backgroundColor: backgroundColor}
+	}
 
 	return (
-		<div className={"container_background"}>
+		<div className={"container_background"} style={{width: window.innerWidth}}>
 			<div className={"container_headband"}>
-				<button className={"text_headband"} style={{marginLeft: '0%'}}>Werewolf</button>
+				<button className={"text_headband"} style={{marginRight: '0%'}}>Werewolf</button>
 				<button className={"text_headband"}>Time Bomb</button>
 				<button className={"text_headband"}>Maze Adventure</button>
 			</div>
-			<div className={"container_body"} style={{height: window.innerHeight}}>
-				<h2 className={"name"}>Simon Bauchet</h2>
-				<div style={{backgroundColor: '#FFF', height: window.innerHeight}}>
-					<p style={{color: '##7D7F7B'}}>Etudiant à Epitech en 4ème année, je suis passionné de l'informatique.</p>
-				</div>
-				<div style={{backgroundColor: '#FF4655', height: window.innerHeight+window.innerHeight*0.05, padding: 5, display: 'flex', width: 'fit-content', flexDirection: isSmallScreenPortraitVideo ? 'column' : ''}}>
-					<div style={{marginTop: window.innerHeight*0.10, width: window.innerWidth, display: 'flex'}}>
-						<ReactPlayer url={'https://youtu.be/hCzs0sIr5lI'} config={{youtube: {embedOptions: {width: 300, height: 635}, playerVars: {showinfo: 0}}}} className={"video_container_portrait"} playing={true} loop={true}/>
-						<div style={{flexDirection: 'column'}}>
-							<p style={{display: 'flex', marginLeft: 15}}>Technologie utilisées: NodeJs, React</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Projet réalisé en 2 semaines pendant mon temps libre.</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Le but de ce projet est de pouvoir jouer au jeux de société Time Bomb n'importe où.</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Je n'ai pas réalisé d'application pour ce projet car je veux que tout le monde puisse y jouer sans rien installer, ainsi toute personne peut se joindre en soirée à une partie par example.</p>
+			<div className={"content_container_phone"}>
+				<div style={div_container_phone("#FF4655")}>
+					<ReactPlayer className={(isSmallScreenPortraitVideo ? "video_container_portrait_phone" : "video_container_portrait")} url={'https://youtu.be/hCzs0sIr5lI'} config={{youtube: {embedOptions: {width: '100%', height: '100%'}, playerVars: {showinfo: 0}}}} playing={true} loop={true}/>
+					<div className={"description_container"}>
+						<div style={{color: "#FFF", display: 'contents'}}>
+							<span className={"background_text"}>Time Bomb</span>
+							<span className={"description_title"}>Time Bomb</span>
+							<div style={{marginBottom: 30}}>
+								<span className={"description_text"} style={{textDecoration: 'underline'}}>Technologie utilisées:</span>
+								<span className={"description_text"}>NodeJs | React</span>
+							</div>
+							<span className={"description_text"}>Projet réalisé en 2 semaines pendant mon temps libre.</span>
+							<span className={"description_text"}>Le but de ce projet est de pouvoir jouer au jeux de société Time Bomb n'importe où.</span>
+							<span className={"description_text"}>Je n'ai pas réalisé d'application pour ce projet car je veux que tout le monde puisse y jouer sans rien installer, ainsi toutes personnes peut se joindre en soirée à une partie par example.</span>
 						</div>
 					</div>
 				</div>
-				<div style={{backgroundColor: '#FFF', height: window.innerHeight, padding: 5, display: 'flex', width: 'fit-content', flexDirection: isSmallScreenPortraitVideo ? 'column' : ''}}>
-					<div style={{marginTop: window.innerHeight*0.10, width: window.innerWidth, display: 'flex'}}>
-					<ReactPlayer url={'https://youtu.be/CGfeq1l48JI'} config={{youtube: {embedOptions: {width: 300, height: 635}, playerVars: {showinfo: 0}}}} className={"video_container_portrait"} playing={true} loop={true}/>
-						<div style={{flexDirection: 'column'}}>
-							<p style={{display: 'flex', marginLeft: 15}}>Technologie utilisées: NodeJs, React</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Projet réalisé en 7 jours pendant mon temps libre.</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Le but de ce projet est de pouvoir jouer au jeux de société Loup Garou nimporte où.</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Il permet d'avoir un mélangeur de rôle afin de pouvoir jouer ou que l'on est.
+
+				<div style={div_container_phone("#FFF")}>
+					<ReactPlayer className={(isSmallScreenPortraitVideo ? "video_container_portrait_phone" : "video_container_portrait")} url={'https://youtu.be/CGfeq1l48JI'} config={{youtube: {embedOptions: {width: '100%', height: '100%'}, playerVars: {showinfo: 0}}}} playing={true} loop={true}/>
+					<div className={"description_container"}>
+						<div style={{color: "#768079", display: 'contents'}}>
+							<span className={"description_title"}>WereWolf / Loup Garou</span>
+							<div style={{marginBottom: 30}}>
+								<span className={"description_text"} style={{textDecoration: 'underline'}}>Technologie utilisées:</span>
+								<span className={"description_text"}>NodeJs | React</span>
+							</div>
+							<span className={"description_text"}>Projet réalisé en 7 jours pendant mon temps libre.</span>
+							<span className={"description_text"}>Le but de ce projet est de pouvoir jouer au jeux de société Loup Garou nimporte où.</span>
+							<span className={"description_text"}>Il permet d'avoir un mélangeur de rôle afin de pouvoir jouer ou que l'on est.
 								Le premier joueur à se connecter est le maître du jeu et peut décider des rôles.
-								Quand une personne meurt, elle peut dévoiler son rôle aux autres joueurs avec le bouton de révélation.</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Je n'ai pas réalisé d'application pour ce projet car je veux que tout le monde puisse y jouer sans rien installer, ainsi toute personne peut se joindre en soirée à une partie par example.</p>
+								Quand une personne meurt, elle peut dévoiler son rôle aux autres joueurs avec le bouton de révélation.</span>
+							<span className={"description_text"}>Je n'ai pas réalisé d'application pour ce projet car je veux que tout le monde puisse y jouer sans rien installer, ainsi toute personne peut se joindre en soirée à une partie par example.</span>
 						</div>
 					</div>
 				</div>
-				<div style={{backgroundColor: '#0F1923', height: window.innerHeight, padding: 5, display: 'flex', width: 'fit-content', alignItems: 'center', flexDirection: isSmallScreenLandscapeVideo ? 'column' : ''}}>
-					<div style={{marginTop: window.innerHeight*0.10, width: window.innerWidth, display: 'flex'}}>
-					<ReactPlayer url={'https://youtu.be/tVsqJKOjwwg'} config={{youtube: {embedOptions: {width: 635, height: 300}, playerVars: {showinfo: 0}}}} className={"video_container_landscape"} playing={true} loop={true}/>
-						<div style={{flexDirection: 'column'}}>
-							<p style={{display: 'flex', marginLeft: 15}}>Technologie utilisées: Unity, C#</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Projet en cours de réalisation.</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Le but du jeux est de trouver la sortie du labyrinthe afin de récolter tous les trésors</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Il y a un système de loot, et de build afin d'aider le joueur dans sa quête.</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Le jeux est en cours de réalisation, c'est pourquoi il peut y avoir des bugs et des features manquantes.</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Dans le jeux, un carré vert représente la sortie, celui de couleur bleu représente la clé de la salle secrète, le carré rouge représente la salle secrète lorsqu'elle est fermée, celle-ci passe au orange une fois la clé récupérée.</p>
-							<p style={{display: 'flex', marginLeft: 15, fontWeight: 'bold'}}>Ce que j'aime:</p>
-							<p style={{display: 'flex', marginLeft: 15}}>- J'ai designé moi même le système de loot afin de pouvoir gérer le taux de drop de chaque item.</p>
-							<p style={{display: 'flex', marginLeft: 15}}>- Le système de build, autant la conception pour réaliser quelque chose d'équilibré que la réalisation qui réserve ses petits imprévus !</p>
-							<p style={{display: 'flex', marginLeft: 15}}>Pour le tester &nbsp; <a href={'https://simmer.io/@cryan/mazeadventure'} target={'_blank'}> cliquez ici</a></p>
+
+				<div style={div_container_phone("#0F1923")}>
+					<div className={"description_container"}>
+						<div style={{color: "#FFF", display: 'contents'}}>
+							<span className={"description_title"}>MazeAdventure</span>
+							<div style={{textAlign: 'center', marginBottom: 30}}>
+								<div style={{display: "inline-block"}}>
+									<ReactPlayer className={(isSmallScreenLandscapeVideo ? "video_container_landscape_phone" : "video_container_phone")}  url={'https://youtu.be/tVsqJKOjwwg'} config={{youtube: {embedOptions: {width: '100%', height: '100%'}, playerVars: {showinfo: 0}}}} playing={true} loop={true}/>
+								</div>
+							</div>
+							<div style={{marginBottom: 30}}>
+								<span className={"description_text"} style={{textDecoration: 'underline'}}>Technologie utilisées:</span>
+								<span className={"description_text"}>Unity | C#</span>
+							</div>
+							<span className={"description_text"}>Projet en cours de réalisation.</span>
+							<span className={"description_text"}>Le but du jeux est de trouver la sortie du labyrinthe afin de récolter tous les trésors</span>
+							<span className={"description_text"}>Il y a un système de loot, et de build afin d'aider le joueur dans sa quête.</span>
+							<span className={"description_text"}>Le jeux est en cours de réalisation, c'est pourquoi il peut y avoir des bugs et des features manquantes.</span>
+							<span className={"description_text"}>Dans le jeux, un carré vert représente la sortie, celui de couleur bleu représente la clé de la salle secrète, le carré rouge représente la salle secrète lorsqu'elle est fermée, celle-ci passe au orange une fois la clé récupérée.</span>
+							<span className={"description_text"} style={{textDecoration: 'underline'}}>Ce que j'aime:</span>
+							<span className={"description_text"}>- J'ai designé moi même le système de loot afin de pouvoir gérer le taux de drop de chaque item.</span>
+							<span className={"description_text"}>- Le système de build, autant la conception pour réaliser quelque chose d'équilibré que la réalisation qui réserve ses petits imprévus !</span>
+							<span className={"description_text"}>Pour le tester &nbsp; <a href={'https://simmer.io/@cryan/mazeadventure'} target={'_blank'}> cliquez ici</a></span>
 						</div>
 					</div>
 				</div>
